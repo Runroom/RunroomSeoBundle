2929ad911da22c1e1c359d21a645568a1e219fa6
Jordi Sala Morales <jordism91@gmail.com>
2020-07-08 10:20:11 +0200

    add missing test (#63)

--- tests/App/Entity/MetaInformationAwareEntity.php
+++ tests/App/Entity/MetaInformationAwareEntity.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Runroom\SeoBundle\Tests\Fixtures;
+namespace Runroom\SeoBundle\Tests\App\Entity;
 
 use Runroom\SeoBundle\Behaviors\MetaInformationAware;
 
